dcf974b18396c147f17e61961fda8ef505c7ef39
andersonfsgit <afs10@cin.ufpe.br>
2018-04-22 22:03:47 -0300

    Retirada de uma pasta.

--- GeoSaude-App/src/app/controllers/generic-controller.ts
+++
@@ -1,9 +0,0 @@
-export class GenericController{
-
-  getCurrentUser(){
-    return {
-      name: 'nome',
-      schedule: [[-8.05362071, -34.95265961, true], [-8.05352071, -34.95265961, false], [-8.05361071, -34.95264961, true]]
-    }
-  }
-}
--- GeoSaude-App/src/app/models/scheduled-location-model.ts
+++
@@ -1,8 +0,0 @@
-import { Location } from "./location-model";
-
-export class ScheduledLocation {
-
-    constructor(public location: Location, public date: Date, public cicle: string){
-    }
-
-}
