467a7595258f64d754825a9f5c3ea9ea4ac913e3
Anderson Felix <anderson.ffelixsilva@gmail.com>
2018-12-28 01:25:29 -0300

    novo projeto - geocode

---
+++ geocode/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
+import { GeocoderProvider } from '../providers/geocoder/geocoder';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    NativeGeocoder,
+    GeocoderProvider
+  ]
+})
+export class AppModule {}
